b944ba87865fc6ff5c993137a3d1e6237ac7b1dd
wellupgeek <1638098056@qq.com>
2020-09-10 16:13:09 +0800

    代码逻辑设计

--- src/api_spider.py
+++ src/api_spider.py
@@ -11,7 +11,6 @@ class Web_api:
         self.subproj_url = base_ip + "/changes/?O=81&S={}&n=25&q=project%3A{}"
         self.detail_url = base_ip + "/changes/{}~{}/detail?O=516714"
         self.files_url = base_ip + "/changes/{}~{}/revisions/{}/files"
-        # self.diff_url = "/{}/diff?context=ALL&intraline&whitespace=IGNORE_NONE"
         self.session = None
 
     def login(self):
@@ -49,6 +48,21 @@ class Web_api:
                 return True
         return False
 
+    def api_judge_in(self, code_type): # 判断变更代码中是否有api接口信息
+        return False
+
+    def api_judge_out(self, code_type):    # 判断变更代码是否属于api接口代码
+        return False
+
+    def judge_code(self, pre_cont, cont, code_type):
+        if not self.api_judge_in():
+            if self.api_judge_out():    # 变更代码中没有，但被包含在api接口代码中的
+                pass
+            else:   # 不在api接口代码中的
+                pass
+        else:   # 变更代码中有api接口的
+            pass
+
     # def save(self, plink_list):
     #     with open("test.txt", "w", encoding="utf-8") as f:
     #         f.write(json.dumps(plink_list, ensure_ascii=False, indent=2))
@@ -69,11 +83,33 @@ class Web_api:
                     files = self.deal_json(self.parse_url(files_url))
                     files_list = files.keys()
                     for f in files_list:    # 遍历每个文件
-                        if f.split(".")[-1] in ["js", "php"]:
+                        suffix = f.split(".")[-1]
+                        if suffix in ["js", "php"]:
                             if self.path_judge(f):
                                 diff_url = files_url + "/{}/diff?context=ALL&intraline&whitespace=IGNORE_NONE".format(quote(f, "utf-8"))
                                 diff_data = self.deal_json(self.parse_url(diff_url))    # 开始获取diff数据
-
+                                temp, linea, lineb = [], 0, 0
+                                code_type = "js" if suffix == "js" else "php"
+                                for cont in diff_data["content"]:
+                                    if "ab" in cont:    # 公共代码, 记录代码行号，同时记录
+                                        temp = cont["ab"]
+                                        length = len(temp)
+                                        linea += length
+                                        lineb += length
+                                    elif "a" in cont and "b" not in cont:   # 删除, 判断a, 记录代码行号
+                                        self.judge_code(temp, cont["a"], code_type)
+                                        linea += len(cont["a"])
+                                        pass
+                                    elif "b" in cont and "a" not in cont:   # 新增，判断b，记录代码行号
+                                        self.judge_code(temp, cont["b"], code_type)
+                                        lineb += len(cont["b"])
+                                        pass
+                                    else:   # 修改，调用删除，新增接口
+                                        self.judge_code(temp, cont["a"], code_type)
+                                        linea += len(cont["a"])
+                                        self.judge_code(temp, cont["b"], code_type)
+                                        lineb += len(cont["b"])
+                                        pass
                                 pass
 
         # 关于缓存，我个人的意见是把已经确定的文件链接进行标记（包含patch），然后每次遍历时可以防止重复从而减少时间
