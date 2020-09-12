c18a01c3a61660090e436d1a2489315a16c81703
wellupgeek <1638098056@qq.com>
2020-09-12 20:56:16 +0800

    属于api接口代码判断算法

--- src/api_spider.py
+++ src/api_spider.py
@@ -53,32 +53,83 @@ class Web_api:
         for line, code_str in enumerate(cont):
             r = regex.search(code_str)
             if r:
-                return r[0], True, line
-        return "", False, len(cont)
+                return r[0], r.end(), True, line
+        return "", -1, False, len(cont)
+
+    def judge_in_api(self, flag, endpos, cont): # 此处的cont就是从api标志往下一直到变更代码开头
+        if flag == "api.":   # api.function(...).then(...) | $http(...).then(...) or .catch(...) .finally()
+        # 一般来说是 api.function().then()|catch()|finally(), 如果cont遍历完，括号没匹配完则说明变更代码包含在api接口代码中，否则就是不包含
+            # 先找到api.后面的(，然后压入栈中，遇见)就弹出
+            cont[0], stack, flag = cont[0][endpos : len(cont[0])], [], False
+            num = 1
+            for code_str in cont:
+                p = -1
+                for index, c in enumerate(code_str):
+                    if num:
+                        if c == "(":
+                            stack.append("(")
+                            flag = True
+                        if c == ")" and flag:
+                            stack.pop()
+                            p = index
+                        if len(stack) == 0 and flag: # ".then" or ".catch" or ".finally"   api.func().then()
+                            num -= 1
+                            length = len(code_str)
+                            for k in [".then", ".catch", ".finally"]:
+                                if k in code_str[p+1:length]:
+                                    num += 1
+                                    break
+                    else:
+                        break
+                if num == 0:
+                    break
+            if len(stack) == 0: # 意味着stack为空，则说明api接口代码结构已经匹配完成
+                return False
+            else:
+                return True
+
+
+        elif flag == "$.ajax":  # $.ajax(...)
+
+            pass
+        elif flag == "$http":
+            pass
+
+        else:   # flag == "public function"
+
+            pass
+        pass
 
     def api_judge_in(self, cont, code_type, regex_js, regex_php): # 判断变更代码中是否有api接口信息
         self.judge_is_api(regex_js, cont) if code_type == "js" else self.judge_is_api(regex_php, cont)
 
-    def api_judge_out(self, pre_cont, cont, code_type, regex_js, regex_php):    # 判断变更代码是否属于api接口代码
+    def api_judge_out(self, pre_cont, code_type, regex_js, regex_php):    # 判断变更代码是否属于api接口代码
         # 先从pre_cont中倒着找，看能不能找到(看代码长度，可以往前找不超过300行，如果超过忽略)
         length = len(pre_cont)
         start = 0 if length <= 300 else length - 300
-        copy_cont = cont[start:length]
+        copy_cont = pre_cont[start : length + 1]
         copy_cont.reverse()
-        self.judge_is_api(regex_js, copy_cont) if code_type == "js" else self.judge_is_api(regex_php, copy_cont)
+        flag, endpos, status, line = self.judge_is_api(regex_js, copy_cont) if code_type == "js" else self.judge_is_api(regex_php, copy_cont)    # 这里判断是否有api接口代码
+        # 如果有，则开始判断cont代码是否包含在该api接口代码结构中
+        if status:
+            copy_cont2 = copy_cont[0 : line + 1].reverse()
+            self.judge_in_api(flag, endpos, copy_cont2)
+            pass    # 根据不同的代码类型，以及不同的标志来决定不同的代码结构
+        pass
 
-    def get_api_msg(self):  # 确认为api接口代码后，找寻api描述信息
+    def get_api_msg(self):  # 确认为api接口代码后，找寻api描述信息---注释
+
         pass
 
     def judge_code(self, pre_cont, cont, code_type, regex_js, regex_php):
-        flag, status, line = self.api_judge_in(cont, code_type, regex_js, regex_php)
+        flag, endpos, status, line = self.api_judge_in(cont, code_type, regex_js, regex_php)
         if not status:  # 注意line2的行号为ab公共代码从后往前数的行号
-            flag2, status2, line2 = self.api_judge_out(pre_cont, cont, code_type, regex_js, regex_php)    # 变更代码中没有，但被包含在api接口代码中的
+            flag2, endpos, status2, line2 = self.api_judge_out(pre_cont, code_type, regex_js, regex_php)    # 变更代码中没有，但被包含在api接口代码中的
             if status2:
                 self.get_api_msg()
             else:   # 不在api接口代码中的
                 pass
-        else:   # 变更代码中有api接口的
+        else:   # 变更代码中有api接口的，直接找注释信息即可
             self.get_api_msg()
 
     # def save(self, plink_list):
@@ -90,7 +141,8 @@ class Web_api:
         self.session = self.login() # 登陆
         plink_list, pages = self.get_link_list()  # 获取所有的当前页面链接信息, 默认从第一页开始
         # self.save(plink_list) # 测试
-        regex = re.compile(r"\bapi\.|(?<!\w)\$\.ajax(?!\w)|(?<!\w)\$http(?!\w)")    # 将正则对象提前准备好，优化时间
+        regex_js = re.compile(r"\bapi\.|(?<!\w)\$\.ajax(?!\w)|(?<!\w)\$http(?!\w)")    # 将正则对象提前准备好，优化时间
+        regex_php = re.compile(r"\bpublic\s+function(?!\w)") #
         for i, p in enumerate(plink_list):
             for j in p[i + 1]:
                 detail_url = self.detail_url.format(self.proj_name, j["number"])
@@ -116,17 +168,17 @@ class Web_api:
                                         linea += length
                                         lineb += length
                                     elif "a" in cont and "b" not in cont:   # 删除, 判断a, 记录代码行号
-                                        self.judge_code(temp, cont["a"], code_type, regex)
+                                        self.judge_code(temp, cont["a"], code_type, regex_js, regex_php)
                                         linea += len(cont["a"])
                                         pass
                                     elif "b" in cont and "a" not in cont:   # 新增，判断b，记录代码行号
-                                        self.judge_code(temp, cont["b"], code_type, regex)
+                                        self.judge_code(temp, cont["b"], code_type, regex_js, regex_php)
                                         lineb += len(cont["b"])
                                         pass
                                     else:   # 修改，调用删除，新增接口
-                                        self.judge_code(temp, cont["a"], code_type, regex)
+                                        self.judge_code(temp, cont["a"], code_type, regex_js, regex_php)
                                         linea += len(cont["a"])
-                                        self.judge_code(temp, cont["b"], code_type, regex)
+                                        self.judge_code(temp, cont["b"], code_type, regex_js, regex_php)
                                         lineb += len(cont["b"])
                                         pass
                                 pass
